4f907128e2e56494d3564fb4fb628c9e5753f222
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-11-07 04:34:15 -0800

    updated category GET requests

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,8 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
-const Product = require('./Product');
-const Tag = require('./Tag');
+
+
 
 class ProductTag extends Model {}
 
@@ -17,14 +17,14 @@ ProductTag.init(
     product_id:{
       type:DataTypes.INTEGER,
       references:{
-        model:Product,
+        model:'Product',
         key:'id',
       },
     },
     tag_id:{
       type:DataTypes.INTEGER,
       references:{
-        model: Tag,
+        model: 'Tag',
         key:'id',
       },
     },
